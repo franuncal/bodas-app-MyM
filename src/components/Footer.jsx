5d076a59272748d56a85014c21dea3f383484512
Fran Uncal <franuncal@gmail.com>
2025-05-18 21:21:02 -0300

    testing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,10 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="footer-copy">Unqui Dev © 2025</p>
+      <p className="footer-copy">© 2025 - Unqui Dev </p>
       <div className="footer-icons">
         <a
-          href="https://instagram.com/tucuenta"
+          href="https://www.instagram.com/unquidevs/"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Instagram"
@@ -16,7 +16,7 @@ const Footer = () => {
           <FaInstagram />
         </a>
         <a
-          href="https://wa.me/541112345678"
+          href="https://wa.me/2324509499?text=Hola! Me comunico desde la página web de Bodas.Click. Quiero recibir más información."
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Whatsapp"
